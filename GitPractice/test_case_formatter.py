9a9987a9e2e618033da2d37d87117dd9d23b7c1b
Sai Prasanna <saiprasannapendem25@gmail.com>
2025-07-23 21:44:10 +0530

    Added test_case_formatter project

---
+++ GitPractice/test_case_formatter.py
@@ -0,0 +1,11 @@
+# Basic Formatter Using Input
+# Taking inputs
+test_case_id = input("Enter Test Case ID: ")
+test_case_title = input("Enter Test Case Title: ")
+test_case_step = input("Enter Test Case Step: ")
+
+# Formatting the test case
+print("\n--- Formatted Test Case ---")
+print(f"Test Case ID   : {test_case_id}")
+print(f"Title          : {test_case_title}")
+print(f"Step(s)        : {test_case_step}")
